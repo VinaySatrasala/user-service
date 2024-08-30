4859d6c789345abb25e1a4c0b11bbc7cef9ee1cd
shahad7 <shahadneeliyatt786@gmail.com>
2024-08-30 23:14:49 +0530

    eureka config

--- src/main/java/com/capstone/user_service/controller/UserController.java
+++ src/main/java/com/capstone/user_service/controller/UserController.java
@@ -7,11 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
+
+    //for gateway testing
+    @GetMapping
+    public String test(){
+        return "user service up and running";
+    }
 
     @Autowired
     public UserController(UserService userService) {
